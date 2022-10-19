30abbdd3431289f844b2a2837c15865da2509966
edvinspapakuls <47062521+edvinspapakuls@users.noreply.github.com>
2022-10-19 16:01:50 +0100

    Coded my solution
    Coded my solution

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 '''
-Product:
-Description:
-Author:
+Product: Two_Sum
+Description: Two sum generator
+Author: Edvin papakul
 '''
 from generator import listClass
 
@@ -14,9 +14,17 @@ class Solution():
         '''
         Write you solution in here!
         '''
+        target = 9
+        list1 = []
+        for i in range(len(self.listOfNumbers)):
+            for j in self.listOfNumbers:
+                if j != self.listOfNumbers[i] and j + self.listOfNumbers[i] == target:
+                    list1.append((self.listOfNumbers[i], j))
+        print(list1)
         
         pass
 
 if __name__ == "__main__":
     inst = Solution()
     inst.your_solution()
+    
